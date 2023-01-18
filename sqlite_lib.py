515c824adeec94ed03fa5e4fc5852ea3bc4dd728
Valery-77 <mail0972464642@gmail.com>
2023-01-18 10:45:10 +0200

    me
    init

--- sqlite_lib.py
+++ sqlite_lib.py
@@ -6,25 +6,28 @@ table_name = 'bot_userdata'
 
 
 async def db_start():
-    global db, crs
     db = sql.connect(db_name)
     crs = db.cursor()
-    crs.execute(f"""CREATE TABLE IF NOT EXIST {table_name} (registration TEXT, channel TEXT, id INTEGER PRIMARY KEY, 
-    username TEXT, first_nameTEXT, last_name TEXT, email TEXT, mobile TEXT,	link TEXT, utm_source TEXT,
-    utm_campaign TEXT, utm_medium TEXT, utm_term TEXT, utm_content TEXT, last_visit TEXT)""")
+    crs.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (registration datetime, channel TEXT, id INTEGER PRIMARY 
+    KEY, username TEXT, first_name TEXT, last_name TEXT, email TEXT, mobile TEXT, link TEXT, utm_source TEXT, 
+    utm_campaign TEXT, utm_medium TEXT, utm_term TEXT, utm_content TEXT, last_visit datetime)""")
     db.commit()
     crs.close()
 
 
-async def user_connect(user_id):
+async def user_connect(msg):
+    db = sql.connect(db_name)
+    crs = db.cursor()
+    user_id = msg.from_user.id
+    cur_date = datetime.date.today()
     user = crs.execute(f"SELECT 1 FROM {table_name} WHERE id == {user_id}").fetchone()
-    cur_date = str(datetime.date.today())
     if not user:
+        fst_name = msg.from_user.first_name
+        lst_name = msg.from_user.last_name
+        user_name = msg.from_user.username
         crs.execute(f"INSERT INTO {table_name} VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                    (cur_date, '', user_id, '', '', '', '', '', '', '', '', '', '', '', cur_date))
-        db.commit()
-        crs.close()
+                    (cur_date, '', user_id, user_name, fst_name, lst_name, '', '', '', '', '', '', '', '', cur_date))
     else:
         crs.execute(f"UPDATE {table_name} SET last_visit = {cur_date} WHERE id == {user_id}")
-        db.commit()
-        crs.close()
+    db.commit()
+    crs.close()
--- tg_bot.py
+++ tg_bot.py
@@ -1,16 +1,22 @@
 from aiogram import Bot, executor, Dispatcher, types
-from sqlite_lib import db_start as start
+from sqlite_lib import db_start, user_connect
+
 token = '5839626263:AAH6mCa4c72LBzF5Lj9DiRX7az3AT7SMZqs'
 
 bot = Bot(token=token)
 disp = Dispatcher(bot=bot)
 
+
+async def on_startup(_):
+    await db_start()
+
+
 @disp.message_handler()
-async def send_echo(msg: types.Message) -> None:
-    print(msg)
-    await msg.answer(msg.text)
+async def record_data(msg: types.Message) -> None:
+    # print(msg)
+    await user_connect(msg)
+    await msg.answer(msg.text)  # ECHO
 
 
 if __name__ == '__main__':
-    start()
-    executor.start_polling(disp)
+    executor.start_polling(disp, on_startup=on_startup)
